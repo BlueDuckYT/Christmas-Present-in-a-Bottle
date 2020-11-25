d49bd6e74a340c6ac7ede0a5560bf121da357483
Max <bludux595@gmail.com>
2020-11-25 10:04:53 -0500

    Fixed crash

--- src/main/java/blueduck/christmas_wish_in_a_bottle/registry/Registry.java
+++ src/main/java/blueduck/christmas_wish_in_a_bottle/registry/Registry.java
@@ -7,6 +7,7 @@ import blueduck.jellyfishing.jellyfishingmod.blocks.JellyBlock;
 import blueduck.jellyfishing.jellyfishingmod.items.SpatulaItem;
 import blueduck.jellyfishing.jellyfishingmod.registry.BlockItemBase;
 import blueduck.jellyfishing.jellyfishingmod.registry.JellyfishingBlocks;
+import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
@@ -25,7 +26,7 @@ public class Registry {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, "christmas_wish_in_a_bottle");
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, "christmas_wish_in_a_bottle");
 
-    public static final RegistryObject<Block> BOTTLE_LAUNCHER = BLOCKS.register("bottle_launcher", () -> new BottleLauncherBlock(Block.Properties.from(JellyfishingBlocks.SCRAP_METAL.get()).notSolid()));
+    public static final RegistryObject<Block> BOTTLE_LAUNCHER = BLOCKS.register("bottle_launcher", () -> new BottleLauncherBlock(AbstractBlock.Properties.create(Material.IRON, MaterialColor.BROWN).sound(SoundType.METAL).hardnessAndResistance(2.0F, 2.0F).harvestTool(ToolType.PICKAXE).harvestLevel(2).notSolid()));
     public static final RegistryObject<Item> BOTTLE_LAUNCHER_ITEM = ITEMS.register("bottle_launcher", () -> new BlockItem(BOTTLE_LAUNCHER.get(), new Item.Properties().group(ItemGroup.MISC)));
 
     public static final RegistryObject<Item> WISH_IN_A_BOTTLE = ITEMS.register("christmas_wish_in_a_bottle", () -> new WishInBottleItem(new Item.Properties().group(ItemGroup.MISC)));
